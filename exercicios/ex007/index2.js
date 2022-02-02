df8f76ca94fac0877aef0099e5883499f30bdc86
Ângelo Gabriel <gabrielpalhano.dev@gmail.com>
2022-02-01 21:01:55 -0300

    Aula 11 parte 01

---
+++ exercicios/ex007/index2.js
@@ -0,0 +1,7 @@
+var pais = 'Korea'
+console.log(`Vivo em ${pais}`)
+    if (pais == 'Brasil') {
+        console.log('Você é Brasileiro')
+    } else {
+        console.log('Você é Estrangeiro')
+    }
